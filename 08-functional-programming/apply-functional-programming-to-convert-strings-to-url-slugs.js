e62154c7b85d453bd5d3436a20e7da39ea89c1a0
kasi <danielkaser@mailbox.org>
2023-10-10 20:03:03 +0200

    solve apply-functional-programming-to-convert-strings-to-url-slugs.js

---
+++ 08-functional-programming/apply-functional-programming-to-convert-strings-to-url-slugs.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+function urlSlug(title) {
+
+  return title
+    .toLowerCase()
+    .split(/\s/)
+    .filter(word => word) // remove (falsy) spaces
+    .join('-')
+
+}
+// Only change code above this line
+urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
