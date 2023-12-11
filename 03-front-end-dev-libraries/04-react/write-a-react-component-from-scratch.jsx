c1f1551be89f3abdfa7b23a5633e87e1137f22e7
kasi <danielkaser@mailbox.org>
2023-12-11 20:06:23 +0100

    solve write-a-react-component-from-scratch.jsx

---
+++ 03-front-end-dev-libraries/04-react/write-a-react-component-from-scratch.jsx
@@ -0,0 +1,19 @@
+// Change code below this line
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My First React Component!</h1>
+      </div>
+    );
+  }
+}
+
+const challengeNode = document.querySelector("#challenge-node");
+
+ReactDOM.render(<MyComponent />, challengeNode);
