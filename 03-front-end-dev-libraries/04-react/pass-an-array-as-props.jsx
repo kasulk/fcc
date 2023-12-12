d4666da5546777604f0ee04a06f667ce9421ef6b
kasi <danielkaser@mailbox.org>
2023-12-12 19:42:33 +0100

    solve pass-an-array-as-props.jsx

---
+++ 03-front-end-dev-libraries/04-react/pass-an-array-as-props.jsx
@@ -0,0 +1,30 @@
+const List = (props) => {
+  {
+    /* Change code below this line */
+  }
+  return <p>{props.tasks.join(", ")}</p>;
+  {
+    /* Change code above this line */
+  }
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today</h2>
+        {/* Change code below this line */}
+        <List tasks={["walk dog", "workout"]} />
+        <h2>Tomorrow</h2>
+        <List
+          tasks={["wash dishes", "kiss girlfriend", "fold a piece of paper"]}
+        />
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+}
