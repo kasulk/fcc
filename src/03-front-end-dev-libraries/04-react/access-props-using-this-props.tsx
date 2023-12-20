9b2f47b735fcb674d610592f5824ddb3c0d96091
kasi <danielkaser@mailbox.org>
2023-12-20 16:56:01 +0100

    typify access-props-using-this-props.tsx

--- src/03-front-end-dev-libraries/04-react/access-props-using-this-props.tsx
+++ src/03-front-end-dev-libraries/04-react/access-props-using-this-props.tsx
@@ -1,5 +1,12 @@
-class App extends React.Component {
-  constructor(props) {
+import React from "react";
+
+interface IAppProps {}
+interface IWelcomeProps {
+  name: string;
+}
+
+export class App extends React.Component {
+  constructor(props: IAppProps) {
     super(props);
   }
   render() {
@@ -13,8 +20,8 @@ class App extends React.Component {
   }
 }
 
-class Welcome extends React.Component {
-  constructor(props) {
+class Welcome extends React.Component<IWelcomeProps> {
+  constructor(props: IWelcomeProps) {
     super(props);
   }
   render() {
