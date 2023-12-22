2eb758a9ea1fa5d34a01de009432377a68b4eeb6
kasi <danielkaser@mailbox.org>
2023-12-22 17:50:04 +0100

    typify introducing-inline-styles.jsx

--- src/03-front-end-dev-libraries/04-react/introducing-inline-styles.tsx
+++ src/03-front-end-dev-libraries/04-react/introducing-inline-styles.tsx
@@ -1,4 +1,6 @@
-class Colorful extends React.Component {
+import React from "react";
+
+export class Colorful extends React.Component {
   render() {
     return <div style={{ color: "red", fontSize: 72 }}>Big Red</div>;
   }
