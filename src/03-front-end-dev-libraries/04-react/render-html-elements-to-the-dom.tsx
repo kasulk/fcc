e5b8d8490778d81d875900b98fd3c703c1ca37ee
kasi <danielkaser@mailbox.org>
2023-12-29 15:46:48 +0100

    typify render-html-elements-to-the-dom.tsx

---
+++ src/03-front-end-dev-libraries/04-react/render-html-elements-to-the-dom.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const JSX = () => (
+  <div>
+    <h1>Hello World</h1>
+    <p>Lets render this to the DOM</p>
+  </div>
+);
+// Add your code below this line
+
+ReactDOM.render(JSX(), document.getElementById("challenge-node"));
