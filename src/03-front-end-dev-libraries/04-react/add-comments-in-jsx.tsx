4a3e81fedc8ca46863b9b8970ff39072b7ab4e2c
kasi <danielkaser@mailbox.org>
2023-12-29 13:49:48 +0100

    fix fast refresh warning in add-comments-in-jsx.tsx

--- src/03-front-end-dev-libraries/04-react/add-comments-in-jsx.tsx
+++ src/03-front-end-dev-libraries/04-react/add-comments-in-jsx.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const JSX = (
+export const JSX = () => {
   <div>
     {/* This is an awesome comment! */}
     <h1>This is a block of JSX</h1>
     <p>Here's a subtitle</p>
-  </div>
-);
+  </div>;
+};
