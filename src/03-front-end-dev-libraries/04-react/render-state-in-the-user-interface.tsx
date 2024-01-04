8ffc73c0fccd48e3c9b8f17790afba234ac6ebd1
kasi <danielkaser@mailbox.org>
2024-01-04 17:14:15 +0100

    typify render-state-in-the-user-interface.tsx

--- src/03-front-end-dev-libraries/04-react/render-state-in-the-user-interface.tsx
+++ src/03-front-end-dev-libraries/04-react/render-state-in-the-user-interface.tsx
@@ -1,5 +1,15 @@
-class MyComponent extends React.Component {
-  constructor(props) {
+import React from "react";
+
+interface IMyComponentProps {}
+interface IMyComponentState {
+  name: string;
+}
+
+export class MyComponent extends React.Component<
+  IMyComponentProps,
+  IMyComponentState
+> {
+  constructor(props: IMyComponentProps) {
     super(props);
     this.state = {
       name: "freeCodeCamp",
